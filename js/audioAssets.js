4034fe0255dbe0e639ba0e67084d248713708716
Paul Kline <pauliankline@gmail.com>
2017-05-20 17:03:46 -0500

    changed to kjhk stream info

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -3,12 +3,12 @@
 // Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
 var audioData = [
     {
-        'title' : 'Episode 140',
-        'url' : 'https://feeds.soundcloud.com/stream/275202399-amazon-web-services-306355661-amazon-web-services.mp3'
+        'title' : 'high stream',
+        'url' : 'http://173.45.231.37:8000/stream_high'
     },
     {
-        'title' : 'Episode 139',
-        'url' : 'https://feeds.soundcloud.com/stream/274166909-amazon-web-services-306355661-aws-podcast-episode-139.mp3'
+        'title' : 'low stream',
+        'url' : 'http://173.45.231.37:8000/stream_low'
     }
 ];
 
